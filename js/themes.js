c9a19ee5f672aee3593e2bdbcef8b78a1a2f88d4
BisiDavi <oludavidconnect@gmail.com>
2020-12-25 23:56:07 +0100

    themes

--- js/themes.js
+++ js/themes.js
@@ -1,14 +1,18 @@
 let theme = false;
 
 const toggleButton = () => {
-  theme = true;
-  
+  theme = !theme;
+
   switch (theme) {
     case true:
       document.getElementById('nav').classList.add('darkMode');
+      document.getElementById('switchButtonWhite').src =
+        './images/switchbuttonBlue.png';
       break;
     case false:
       document.getElementById('nav').classList.remove('darkMode');
+      document.getElementById('switchButtonWhite').src =
+        './images/switch.png';
       break;
   }
 };
