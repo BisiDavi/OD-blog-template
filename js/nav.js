1223c7a20eee7a874a9c2b3d2476f47129571ff7
BisiDavi <oludavidconnect@gmail.com>
2020-12-25 08:22:28 +0100

    navbar menu

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,25 @@
-let nav = $('.nav');
-let scrolled = false;
+let navMenuState = false;
+let sideNavWidth = 0;
+function openNav() {
+  const sideNav = document.getElementById('sidenav');
+  sideNavWidth = sideNav.style.width = '250px';
+  console.log('sideNavWidth', sideNavWidth);
+  document.getElementById('main').style.marginLeft = '250px';
+  sideNavWidth.length > 2 ? (navMenuState = true) : null;
+  console.log('navMenuState', navMenuState);
+  return navMenuState;
+}
 
-$(window).scroll(function () {
-  if (100 < $(window).scrollTop() && !scrolled) {
-    nav.addClass('visible').animate({ top: '30px' });
-    scrolled = true;
-  }
-  if (100 > $(window).scrollTop() && scrolled) {
-    nav.animate({ top: '-50px' });
-    setTimeout(function () {
-      nav.removeClass('visible');
-    }, 500);
-    scrolled = false;
-  }
-});
+const navOverlay = document.getElementById('sidebarOverlay');
+const navActive = navOverlay.classList.add('active');
+
+navMenuState
+  ? console.log('navActive', navOverlay)
+  : console.log('navRemove', navOverlay.classList.remove('active'));
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById('sidenav').style.width = '0';
+  document.getElementById('main').style.marginLeft = '0';
+  document.body.style.backgroundColor = 'white';
+}
