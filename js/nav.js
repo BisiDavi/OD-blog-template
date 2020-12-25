968a32641fa907e3f6537d2c903873da09ee52e3
BisiDavi <oludavidconnect@gmail.com>
2020-12-25 11:11:49 +0100

    index page

--- js/nav.js
+++ js/nav.js
@@ -3,14 +3,21 @@ let sideNavWidth = 0;
 
 function openNav() {
   const sideNav = document.getElementById('sidenav');
+  const header = document.getElementById("mainHeader");
   sideNavWidth = sideNav.style.width = '250px';
   console.log('sideNavWidth', sideNavWidth);
   document.getElementById('main').style.marginLeft = '250px';
   sideNavWidth.length > 2 ? (navMenuState = true) : null;
 
-  return navMenuState
-    ? document.getElementById('sidebarOverlay').classList.add('active')
-    : null;
+
+  switch(navMenuState){
+    case true: 
+      document.getElementById('sidebarOverlay').classList.add('active');
+      document.getElementById("mainHeader").classList('active');
+      break;
+    default:
+      break;
+  }  
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
