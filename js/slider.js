b5b5e6238bb5fa3e7c92e8b62b71b715d8b4f849
BisiDavi <oludavidconnect@gmail.com>
2020-12-28 22:29:58 +0100

    added owl carousel

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,18 @@
-$(document).ready(function(){
-  $('.slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000
+let owl = $('.owl-carousel');
+
+$(document).ready(function () {
+  owl.owlCarousel({
+    loop: true,
+    margin: 25,
+    dotsEach: true,
+    items: 3
   });
+});
+
+$('.nextButton').click(function () {
+  owl.trigger('next.owl.carousel');
+});
+
+$('.prevButton').click(function () {
+  owl.trigger('prev.owl.carousel', [300]);
 });
